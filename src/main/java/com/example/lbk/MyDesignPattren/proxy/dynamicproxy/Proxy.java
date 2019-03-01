6e019a728add74cd0ffca1eca90af6b65ed534e5
liubokun1992 <liubokun1992@126.com>
2019-03-01 15:51:36 +0800

    引入javapoet

---
+++ src/main/java/com/example/lbk/MyDesignPattren/proxy/dynamicproxy/Proxy.java
@@ -0,0 +1,12 @@
+package com.example.lbk.MyDesignPattren.proxy.dynamicproxy;
+
+import java.io.IOException;
+
+public class Proxy {
+	
+	public static Object newProxyInstance() throws IOException {
+		
+		return null;
+	}
+
+}
