924201adb3ec27660d9a8b24cc9f55b8f31488bb
liubokun1992 <liubokun1992@126.com>
2019-03-01 16:21:55 +0800

    test push

--- src/main/java/com/example/lbk/MyDesignPattren/proxy/ProxyController.java
+++ src/main/java/com/example/lbk/MyDesignPattren/proxy/ProxyController.java
@@ -1,4 +1,4 @@
-package com.example.lbk.MyDesignPattren.Controller;
+package com.example.lbk.MyDesignPattren.proxy;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/lbk/MyDesignPattren/proxy/dynamicproxy/Proxy.java
+++ src/main/java/com/example/lbk/MyDesignPattren/proxy/dynamicproxy/Proxy.java
@@ -2,9 +2,14 @@ package com.example.lbk.MyDesignPattren.proxy.dynamicproxy;
 
 import java.io.IOException;
 
+import com.example.lbk.MyDesignPattren.proxy.staticproxy.Flyable;
+import com.squareup.javapoet.TypeSpec;
+
 public class Proxy {
 	
 	public static Object newProxyInstance() throws IOException {
+		TypeSpec.Builder typeSepcBuilder = TypeSpec.classBuilder("TimeProxy")
+				.addSuperinterface(Flyable.class);
 		
 		return null;
 	}
